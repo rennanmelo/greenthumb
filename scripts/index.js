723b8760ff5149d62c1652429ed6bf4fb65ed76c
Rennan Melo <rennan.melo11@gmail.com>
2021-08-01 18:52:35 -0300

    Created base of the fetchData function

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  const fetchData = (params) => {
+    fetch(`https://front-br-challenges.web.app/api/v2/green-thumb/?sun=${params.sun}&water=${params.water}&pets=${params.pets}`)
+      .then(function (response) {
+        // The API call was successful!
+        return response.json();
+      }).then(function (data) {
+        // This is the JSON from our response
+        console.log(data);
+      }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+      });
+    }
+  
   const buildFilter = () => {
     let params = {};
 
