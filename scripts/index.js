accde589c7871e0745758d180786568b4a1a8c83
Rennan Melo <rennan.melo11@gmail.com>
2021-08-02 19:50:46 -0300

    Created smooth scroll behavior

--- scripts/index.js
+++ scripts/index.js
@@ -99,4 +99,18 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   buildFilter();
+
+  const scrollBehavior = () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  }
+
+  scrollBehavior();
 });
